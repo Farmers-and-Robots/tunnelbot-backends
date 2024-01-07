df51f8548a4be01ce5c04126e0bdfcdb6f7bf5ec
munjeli <1256674+munjeli@users.noreply.github.com>
2024-01-07 06:09:47 -0600

    feat: added deps and mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Farmers-and-Robots/tunnelbot-backends
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Farmers-and-Robots/tunnelbot-backends/models"
+	"github.com/Farmers-and-Robots/tunnelbot-backends/router"
+)
+
+func main() {
+	fmt.Sprintf("%v", models.Address{
+		City:   "Dubuque",
+		State:  "IA",
+		Street: "401 S. Main St",
+		Zip:    "52001",
+	})
+
+	router.NewRouter()
+}
